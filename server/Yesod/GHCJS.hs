54d00f9d64d60deac2ffc5e35305eaf0c8683262
Michael Sloan <mgsloan@gmail.com>
2015-05-03 23:05:48 -0700

    With -fdev, make GHCJS code recompile every page load

--- server/Yesod/GHCJS.hs
+++ server/Yesod/GHCJS.hs
@@ -1,45 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | GHCJS support for Yesod.
 
 module Yesod.GHCJS
     (ghcjsFileDev)
     where
 
-import Data.Char
-import Data.Conduit.Process
-import Data.FileEmbed (embedFile)
-import Language.Haskell.TH
-import Language.Haskell.TH.Syntax
-import Prelude
--- import Settings
-import System.Directory
-import System.Environment
-import Data.Maybe
-import System.Exit
-import System.IO
-import System.IO.Temp
-import Yesod.Core
+import           Control.Monad.Trans.Resource (runResourceT)
+import qualified Data.ByteString as BS
+import           Data.Char
+import           Data.Conduit
+import qualified Data.Conduit.Combinators as CL
+import           Data.Conduit.Process
+import           Data.FileEmbed (embedFile)
+import           Data.Maybe
+import           Data.String (fromString)
+import           Filesystem.Path (hasExtension)
+import qualified Filesystem.Path.CurrentOS as FP
+import           Language.Haskell.TH
+import           Language.Haskell.TH.Syntax
+import           Prelude
+import           System.Directory
+import           System.Environment
+import           System.Exit
+import           System.IO
+import           System.IO.Temp
+import           Yesod.Core
 
 -- | Make a widget by compiling the module to JS to a temporary file
 -- and then loading in the JS. Results in a @Handler TypedContent@
 -- expression.
-ghcjsFileDev :: Bool -> [String] -> FilePath -> Q Exp
-ghcjsFileDev development args fp =
-  do qAddDependentFile fp -- FIXME really need deep dependency tracking
-     let dir = "dist/ghcjs-cache/"
-         genfp = dir ++ slugize fp ++ ".js"
-     runIO (do createDirectoryIfMissing True dir
-               runGhcJSAll
-                 args
-                 fp
-                 (\f ->
-                    do putStrLn ("Copying " ++ f ++ " to " ++ genfp ++ " ...")
-                       copyFile f genfp))
-     if development
-         then [|sendFile typeJavascript genfp|]
-         else do
+ghcjsFileDev :: Bool -> [String] -> [FilePath] -> FilePath -> Q Exp
+ghcjsFileDev development args folders fp = do
+    let args' = map (\folder -> "-i" ++ folder) folders ++ args
+    genfp <- runIO $ runGhcjs fp args'
+    if development
+        then [| liftIO $ do
+            runGhcjs fp args'
+            content <- BS.readFile genfp
+            return $ TypedContent typeJavascript $ toContent content |]
+        else do
+            sourceFiles <- runIO $ runResourceT $
+              mapM_ (CL.sourceDirectoryDeep False . fromString) folders $=
+              CL.filter (`hasExtension` "hs") $$
+              CL.sinkList
+            mapM_ (qAddDependentFile . FP.encodeString) sourceFiles
             bs <- embedFile genfp
             f <- [|return . TypedContent typeJavascript . toContent|]
             return $ f `AppE` bs
+
+runGhcjs :: FilePath -> [String] -> IO FilePath
+runGhcjs fp args = do
+  let dir = "dist/ghcjs-cache/"
+      genfp = dir ++ slugize fp ++ ".js"
+  createDirectoryIfMissing True dir
+  runGhcJSAll args fp $ \f -> do
+    putStrLn ("Copying " ++ f ++ " to " ++ genfp ++ " ...")
+    copyFile f genfp
+  return genfp
 
 -- | Make a slug from a file path.
 slugize :: FilePath -> String
