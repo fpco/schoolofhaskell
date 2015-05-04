cc7a0b760403a3bffba7cd7ebca650b587df2156
Michael Sloan <mgsloan@gmail.com>
2015-05-03 22:18:48 -0700

    Initial commit: add dependencies

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+schoolofhaskell changelog
+=========================
---
+++ scripts/build-ghcjs-deps.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Run ./scripts/create-ghcjs-cabal-config.sh before this.
+#
+# Run this script from the schoolofhaskell directory, like so:
+#
+# ./scripts/build-ghcjs-deps.sh
+
+set -x
+
+fpbuild exec -- $PWD/scripts/build-ghcjs-deps-internal.sh
