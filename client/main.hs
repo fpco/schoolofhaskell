9066cf9b4c14a87517b08a70ceccaa6bcb8ccba1
Michael Sloan <mgsloan@gmail.com>
2015-05-16 22:23:39 -0700

    More consistent indentation

--- client/main.hs
+++ client/main.hs
@@ -12,15 +12,15 @@ import           View
 -- rendering loop.
 main :: IO ()
 main = do
-    app <- getApp
-    ace <- Ace.new app
-    termjs <- TermJs.new app
-    _ <- forkIO $ runApp app
-    -- Just for development
-    -- _ <- forkIO $ do
-    --   state <- readTVarIO (appState app)
-    --   threadDelay (1000 * 1000)
-    --   let editor = Ace.aceEditorOrError (state ^. stateAce)
-    --   code <- Ace.getValue editor
-    --   runCode (appState app) [("main.hs", code)]
-    react app (render ace termjs) =<< getElementById "react-container"
+  app <- getApp
+  ace <- Ace.new app
+  termjs <- TermJs.new app
+  _ <- forkIO $ runApp app
+  -- Just for development
+  -- _ <- forkIO $ do
+  --   state <- readTVarIO (appState app)
+  --   threadDelay (1000 * 1000)
+  --   let editor = Ace.aceEditorOrError (state ^. stateAce)
+  --   code <- Ace.getValue editor
+  --   runCode (appState app) [("main.hs", code)]
+  react app (render ace termjs) =<< getElementById "react-container"
