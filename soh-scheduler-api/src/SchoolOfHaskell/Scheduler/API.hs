6cfd2afcf02f1f63dcca99cdc3915b5d5cb287fc
Michael Sloan <mgsloan@gmail.com>
2015-06-13 18:13:30 -0700

    Derive instances for soh-scheduler-api types

--- soh-scheduler-api/src/SchoolOfHaskell/Scheduler/API.hs
+++ soh-scheduler-api/src/SchoolOfHaskell/Scheduler/API.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
 module SchoolOfHaskell.Scheduler.API where
 
@@ -6,26 +7,32 @@ import           Control.Applicative ((<$>), (<*>))
 import           Control.Lens (makeLenses)
 import           Data.Aeson (ToJSON(..), FromJSON(..))
 import           Data.Aeson.TH (deriveJSON, defaultOptions, fieldLabelModifier)
+import           Data.Data (Data)
 import           Data.Text (Text)
+import           Data.Typeable (Typeable)
 import           Data.UUID.Types (UUID)
 import qualified Data.UUID.Types as UUID
 
 newtype ContainerSpec =
   ContainerSpec {_csImageName :: Text}
+  deriving (Eq, Show, Data, Typeable)
 
 newtype ContainerReceipt =
   ContainerReceipt {_crID :: UUID}
+  deriving (Eq, Data, Typeable)
 
 instance Show ContainerReceipt where
   show = show . _crID
 
 newtype ContainerId =
   ContainerId {_ciID :: Text}
+  deriving (Eq, Show, Ord, Data, Typeable)
 
 data ContainerDetail =
   ContainerDetail {_cdID :: Text
                   ,_cdAddress :: Maybe (Text, Int)
                   ,_cdStatus :: Maybe Text}
+  deriving (Eq, Show, Data, Typeable)
 
 instance ToJSON UUID where
   toJSON = toJSON . UUID.toString
