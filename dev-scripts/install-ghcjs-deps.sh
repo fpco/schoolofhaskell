ff13ce146bc5f00c22240d1a2f044b3774b4e7ea
Michael Sloan <mgsloan@gmail.com>
2015-06-19 00:06:58 -0700

    Move internal script into a dev-scripts/internal dir
    This makes it easier to use tab completion to type
    "./dev-scripts/install-ghcjs-deps.sh"

--- dev-scripts/install-ghcjs-deps.sh
+++ dev-scripts/install-ghcjs-deps.sh
@@ -2,8 +2,8 @@
 
 # Run this script from the schoolofhaskell directory, like so:
 #
-# ./scripts/install-ghcjs-deps.sh
+# ./dev-scripts/install-ghcjs-deps.sh
 
 set -x
 
-stack exec -- $PWD/dev-scripts/install-ghcjs-deps-internal.sh
+stack exec -- $PWD/dev-scripts/internal/install-ghcjs-deps.sh
--- dev-scripts/internal/install-ghcjs-deps.sh
+++ dev-scripts/internal/install-ghcjs-deps.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+# This is an internal script used by dev-scripts/install-ghcjs-deps.sh.
+
 set -x
 
-# This is an internal script used by scripts/build-ghcjs-deps.sh.
 cabal --config-file=cabal-ghcjs-config update
 cabal --config-file=cabal-ghcjs-config install --ghcjs deps/ghcjs-react/ deps/ide-backend/ide-backend-common/ deps/ide-backend-client/ide-backend-json deps/ghcjs-websockets deps/JsonGrammar2 deps/stack-prism deps/language-typescript deps/ghcjs-from-typescript/ghcjs-ace deps/ghcjs-jquery soh-runner-api/ soh-scheduler-api/ --force-reinstalls
