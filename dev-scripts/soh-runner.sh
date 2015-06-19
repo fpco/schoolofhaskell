04070f36701a88b0820a6cc7749ed00c736901a7
Michael Sloan <mgsloan@gmail.com>
2015-06-18 18:07:51 -0700

    Use ENV and ENTRYPOINT in soh-runner Dockerfile
    This makes it easy for the soh-scheduler to pass in the receipt / other
    parameters, without worrying about the details of running soh-runner.

--- dev-scripts/soh-runner.sh
+++ dev-scripts/soh-runner.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-docker run -p 4000:4000 -p 3000:3000 mgsloan/soh-runner:local bash -c 'PATH=/opt/soh/:$PATH soh-runner run --receipt 00000000-0000-0000-0000-000000000000'
+docker run -p 4000:4000 -p 3000:3000 mgsloan/soh-runner:local '00000000-0000-0000-0000-000000000000'
