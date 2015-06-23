6c465587adac92a98d9f25029ac4d4cee2048b44
Michael Sloan <mgsloan@gmail.com>
2015-06-23 00:08:44 -0700

    Remove submodules where the hackage version can be used instead

--- dev-scripts/internal/install-ghcjs-deps.sh
+++ dev-scripts/internal/install-ghcjs-deps.sh
@@ -5,4 +5,4 @@
 set -x
 
 cabal --config-file=cabal-ghcjs-config update
-cabal --config-file=cabal-ghcjs-config install --ghcjs deps/ghcjs-react/ deps/ide-backend/ide-backend-common/ deps/ide-backend-client/ide-backend-json deps/ghcjs-websockets deps/JsonGrammar2 deps/stack-prism deps/language-typescript deps/ghcjs-from-typescript/ghcjs-ace deps/ghcjs-jquery soh-runner-api/ soh-scheduler-api/ --force-reinstalls
+cabal --config-file=cabal-ghcjs-config install --ghcjs deps/ghcjs-react/ deps/ide-backend/ide-backend-common/ deps/ide-backend-client/ide-backend-json deps/JsonGrammar2 deps/ghcjs-from-typescript/ghcjs-ace deps/ghcjs-jquery soh-runner-api/ soh-scheduler-api/
